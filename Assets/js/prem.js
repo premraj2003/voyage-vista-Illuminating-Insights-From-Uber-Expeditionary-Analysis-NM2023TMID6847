76bb8a3464660576d699eb81b3f80e16ac799a03
premraj2003 <143986760+premraj2003@users.noreply.github.com>
2023-10-10 07:36:37 +0000

    Add files via upload

---
+++ Assets/js/prem.js
@@ -0,0 +1,20 @@
+var variable = '' +
+'<html>' +
+'    <head>' +
+'    <title>Your Title Here</title>' +
+'    </head>' +
+'' +
+'    <body bgcolor="FFFFFF">' +
+'        <center><img src="wtools.jpg" align="bottom"> </center>' +
+'        <hr>' +
+'        <a href="http://wtools.io">Best Online Web Tools</a>is a link to another nifty site' +
+'        <h1>This is a Header</h1>' +
+'        <h2>This is a Medium Header</h2>' +
+'        Send me mail at <a href="mailto:support@example.com">support@example.com</a>.' +
+'        <p> This is a new paragraph!</p>' +
+'        <p> <b>This is a new paragraph!</b></p>' +
+'        <br> <b><i>This is a new sentence without a paragraph break, in bold italics.</i></b>' +
+'        <hr>' +
+'    </body>' +
+'</html>' +
+'';
